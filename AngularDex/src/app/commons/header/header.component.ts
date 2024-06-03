9e30c3bf5e87cee07a1cf92d0ce34967de744806
Lucas Pena Araújo dos Santos <lucaspsantos003@gmail.com>
2024-06-03 19:47:37 -0300

    Implementação do Header

--- AngularDex/src/app/commons/header/header.component.ts
+++ AngularDex/src/app/commons/header/header.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule,MatDividerModule,MatListModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  optionsOpen: boolean = false;
+  toggleOptions() {
+    this.optionsOpen = !this.optionsOpen;
+  }
 }
