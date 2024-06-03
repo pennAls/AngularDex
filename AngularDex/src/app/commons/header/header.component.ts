cdede8d79c514de2c22af4be5b74d533a795a65f
Lucas Pena Araújo dos Santos <lucaspsantos003@gmail.com>
2024-06-03 20:28:11 -0300

    Implementação do footer

--- AngularDex/src/app/commons/header/header.component.ts
+++ AngularDex/src/app/commons/header/header.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule,MatDividerModule,MatListModule],
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
@@ -18,4 +24,8 @@ export class HeaderComponent {
   toggleOptions() {
     this.optionsOpen = !this.optionsOpen;
   }
+  goToSearch(): void {
+    console.log("A")
+  }
+ 
 }
