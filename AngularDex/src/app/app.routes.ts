ac3a6330dc67a6d698e0cfd2ccbc751ead81180e
Lucas Pena Araújo dos Santos <lucaspsantos003@gmail.com>
2024-06-04 20:44:12 -0300

    Config Rotas and Modules/Components
    Criação da pasta modules com o pokedex-component, e dentro dele a pasta components com 2 componentes filhos + Config Rotas

--- AngularDex/src/app/app.routes.ts
+++ AngularDex/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SearchPokemonComponent } from './modules/pokedex/components/search-pokemon/search-pokemon.component';
+import { PokedexComponent } from './modules/pokedex/pokedex.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/AngularDex',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'AngularDex',
+    component: PokedexComponent,
+    children: [
+      {
+        path: 'Search-Pokemon',
+        component: SearchPokemonComponent,
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: '/AngularDex',
+    pathMatch: 'full',
+  },
+];
--- AngularDex/src/app/commons/header/header.component.ts
+++ AngularDex/src/app/commons/header/header.component.ts
@@ -4,8 +4,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
-import { FooterComponent } from '../footer/footer.component';
-
 @Component({
   selector: 'app-header',
   standalone: true,
