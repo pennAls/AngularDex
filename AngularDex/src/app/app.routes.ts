73f2d11b027dde36475f13a7de6d67392bd91157
Lucas Pena Araújo dos Santos <lucaspsantos003@gmail.com>
2024-06-04 21:59:38 -0300

    Ajuste de Rotas

--- AngularDex/src/app/app.routes.ts
+++ AngularDex/src/app/app.routes.ts
@@ -2,25 +2,29 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SearchPokemonComponent } from './modules/pokedex/components/search-pokemon/search-pokemon.component';
 import { PokedexComponent } from './modules/pokedex/pokedex.component';
+import { MenuComponent } from './modules/menu/menu.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/AngularDex',
+    redirectTo: 'AngularDex/menu',
     pathMatch: 'full',
   },
 
+  {
+    path: 'AngularDex/menu',
+    component: MenuComponent,
+  },
   {
     path: 'AngularDex',
     component: PokedexComponent,
     children: [
       {
-        path: 'Search-Pokemon',
+        path: 'search-pokemon',
         component: SearchPokemonComponent,
       },
     ],
   },
-
   {
     path: '**',
     redirectTo: '/AngularDex',
--- AngularDex/src/app/modules/pokedex/pokedex.component.ts
+++ AngularDex/src/app/modules/pokedex/pokedex.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-pokedex',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './pokedex.component.html',
   styleUrl: './pokedex.component.css'
 })
